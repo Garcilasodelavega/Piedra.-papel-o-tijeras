ba4f14e3913f2fe5bbfc492180710959851d0f34
Garcilasodelavega <96429488+Garcilasodelavega@users.noreply.github.com>
2024-09-30 09:09:42 +0200

    Update piedra_papel_tijeras.py

--- piedra_papel_tijeras.py
+++ piedra_papel_tijeras.py
@@ -18,3 +18,8 @@ def test_ordenador_decide_jugada():
 # Función principal
 if __name__ == "__main__":
     test_ordenador_decide_jugada()
+
+def usuario_decide_jugada():
+    #Pide al usuario que elija entre piedra, papel o tijeras y devuelve la elección.     
+    eleccion_usuario = input("Elige piedra, papel o tijeras: ")    
+    return eleccion_usuario
